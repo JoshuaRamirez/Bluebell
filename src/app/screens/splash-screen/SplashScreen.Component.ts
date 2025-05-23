c3369333824e13583398f18c1af36061f04620b8
Joshua Ramirez <joshua.ramirez@me.com>
2025-05-23 17:27:31 +0700

    feat: display profile selector on splash

--- src/app/screens/splash-screen/SplashScreen.Component.ts
+++ src/app/screens/splash-screen/SplashScreen.Component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProfileSelectorComponent } from '../../../../libs/ui/components/profile-selector';
 
 @Component({
   selector: 'app-splash-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProfileSelectorComponent],
   templateUrl: './SplashScreen.Template.html',
   styleUrl: './SplashScreen.Styles.scss'
 })
